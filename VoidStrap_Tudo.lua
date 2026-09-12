2c3622b79460838c00313bcdffcaa40f2fff9700
SkyRed01mafia <skyred01mafia@gmail.com>
2026-09-12 16:14:20 -0300

    Add 'AIFA' to BALL_NAMES list

--- VoidStrap_Tudo.lua
+++ VoidStrap_Tudo.lua
@@ -2007,7 +2007,7 @@ local AF_TouchDebounce = 0
 
 local BALL_NAMES = {
     "TPS", "ESA", "MRS", "PRS", "MPS",
-    "Ball", "Football", "Soccer Ball", "Bola", "SoccerBall", "VEF"
+    "Ball", "Football", "Soccer Ball", "Bola", "SoccerBall", "VEF", "AIFA"
 }
 
 local function isBallName(name)
